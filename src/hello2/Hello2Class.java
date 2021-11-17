836c804cfa7e76594b984f792453a6ecfb242757
USER <USER@175.214.135.191>
2021-11-17 15:46:14 +0900

    모험

--- src/hello2/Hello2Class.java
+++ src/hello2/Hello2Class.java
@@ -7,115 +7,61 @@ public class Hello2Class {
 		int a;    // 변수명에 특수문자를 사용할 수 없다.
 		int classa; // 키워드를 변수명으로 사용할 수 없다.
 		
-		String b = "므요"; // 문자열 변수 선언
-		String c = "뿔뱀이";
+		String b = "모험가"; // 문자열 변수 선언
+		String c = "몬스터";
 		a = 100;
-		System.out.println("안녕하세요 ");
-		System.out.println("나는 " + b + " 입니다 ");
-		System.out.println( b + " 잘 부탁드립니다 ");
-		System.out.println( b + "가 길을 가다가 " + c+ "를 만났네 ");
-		System.out.print( b + "와 " + c + "의 호감도는 ");
+		System.out.println( "나는 " + b + " 입니다 ");
+		System.out.println( b + "가 길을 가다가 " + c + "를 만났네 ");
+		System.out.print( b + "의 체력은 ");
 		System.out.println(a);
+		System.out.println();
 		
-		String b1 = "1회";
-		String c1 = "2회";
+		// 몬스터의 마릿수
+		int mon = 5;
+
+		// 몬스터를 죽일 수 있는 횟수
+		// 1번 몬스터는 2번
+		// 2번 몬스터는 3번
+		// 3번 몬스터는 1번
+		// 4번 몬스터는 5번
+		// 5번 몬스터는 10번
+		int[] odds = {2, 3, 1, 5, 10};
 		
-		//나무의 갯수
-		int tree = 5;
-				
-		//나무를 넘길 수 있는 횟수
-		//1번 나무는 2번
-		//2번 나무는 3번
-		//3번 나무는 1번
-		//4번 나무는 5번
-		//5번 나무는 10번
+		//몬스터 번호
+		int monNo = 0;
+		// 몬스터가 5마리
+		// 각 몬스터 번호들을 출력
+		// 몬스터 총 마릿수와 몬스터 끝 번호가 같으면 종료
 		
-		// 5번 나무찍기를 참고 해서 나머지 나무들도 비슷하게 만들어 보자
-		
-		for (int i = 0; i < 2; i++) {
-		    System.out.println("1번 나무를 도끼로 " + (i + 1) + "회 찍었다");
-		    // 만약에 2번이 찍혔으면
-		    if (i == 1) {
-		    // 나무가 넘어간다를 출력
-		    	System.out.println("나무가 넘어간다");
-		    // 2번이 아니면
-		    } else {
-		    // 나무가 넘어가지 않는다를 출력
-		    	System.out.println("나무가 넘어가지 않는다");
-		    }  // else
-		}  // for
-		
-		System.out.print( "남아있는 나무는? ");
-		tree = tree - 1;
-		System.out.println(tree);
-		
-		for (int i = 0; i < 3; i++) {
-		    System.out.println("2번 나무를 도끼로 " + (i + 1) + "회 찍었다");
-		    // 만약에 3번이 찍혔으면
-		    if (i == 2) {
-		    // 나무가 넘어간다를 출력
-		    	System.out.println("나무가 넘어간다");
-		    // 3번이 아니면
-		    } else {
-		    // 나무가 넘어가지 않는다를 출력
-		    	System.out.println("나무가 넘어가지 않는다");
-		    }  // else
-		}  // for
-		
-		System.out.print( "남아있는 나무는? ");
-		tree = tree - 1;
-		System.out.println(tree);
-		
-		for (int i = 0; i < 1; i++) {
-		    System.out.println("3번 나무를 도끼로 " + (i + 1) + "회 찍었다");
-		    // 만약에 1번이 찍혔으면
-		    if (i == 0) {
-		    // 나무가 넘어간다를 출력
-		    	System.out.println("나무가 넘어간다");
-		    // 1번이 아니면
-		    } else {
-		    // 나무가 넘어가지 않는다를 출력
-		    	System.out.println("나무가 넘어가지 않는다");
-		    }  // else
-		}  // for
-		
-		System.out.print( "남아있는 나무는? ");
-		tree = tree - 1;
-		System.out.println(tree);
-		
-		for (int i = 0; i < 5; i++) {
-		    System.out.println("4번 나무를 도끼로 " + (i + 1) + "회 찍었다");
-		    // 만약에 5번이 찍혔으면
-		    if (i == 4) {
-		    // 나무가 넘어간다를 출력
-		    	System.out.println("나무가 넘어간다");
-		    // 5번이 아니면
-		    } else {
-		    // 나무가 넘어가지 않는다를 출력
-		    	System.out.println("나무가 넘어가지 않는다");
-		    }  // else
-		}  // for
-		
-		System.out.print( "남아있는 나무는? ");
-		tree = tree - 1;
-		System.out.println(tree);
-		
-		for (int i = 0; i < 10; i++) {
-		    System.out.println("5번 나무를 도끼로 " + (i + 1) + "회 찍었다");
-		    // 만약에 10번이 찍혔으면
-		    if (i == 9) {
-		    // 나무가 넘어간다를 출력
-		    	System.out.println("나무가 넘어간다");
-		    // 10번이 아니면
-		    } else {
-		    // 나무가 넘어가지 않는다를 출력
-		    	System.out.println("나무가 넘어가지 않는다");
-		    }  // else
-		}  // for
-		
-		System.out.print( "남아있는 나무는? ");
-		tree = tree - 1;
-		System.out.println(tree);
+		// 5번 몬스터 잡기를 참고 해서 나머지 몬스터들도 비슷하게 만들어 보자
+		// 여기 부터 반복
+		while (true) {
+//			monNo++; // 몬스터 번호를 증가
+			for (int i = 1; i <= odds[monNo] ; i++) {
+			    System.out.println((monNo + 1) + "번 몬스터를 " + i + "회 공격했다");
+			    // 만약에 몬스터의 체력만큼 공격 받으면
+			    if (i == odds[monNo]) {
+			        // 몬스터가 죽는다를 출력
+			    	System.out.println("몬스터가 죽는다");
+			    	mon = mon -1; // 몬스터의 수를 줄임
+			        // 체력만큼 공격 받지 않으면
+			    } else {
+			        // 몬스터가 죽지 않는다를 출력
+			    	System.out.println("몬스터가 죽지 않는다");
+			    }  // else
+			}  // for
+		    monNo++;  // 다음 몬스터
+			System.out.print( "남아있는 몬스터는? ");
+			System.out.println(mon);
+			System.out.println();  // 한줄 띄우기
+			// 몬스터 총 마릿수와 몬스터 끝 번호가 같으면 종료
+			// 반복 멈추기
+			if (mon == 0) {
+				break;
+			}  // if
+		}  // while	
+		// 여기까지 반복
+		System.out.println("몬스터 잡기 성공");
 		
 	}  // main
 }  // class
